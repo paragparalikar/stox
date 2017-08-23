2b8509383a7cb8c9d12f41f23dfb60b6d38dcdb2
parag_paralikar <parag_paralikar@PUNE-LAP0607.nuance.com>
2017-08-23 12:28:44 +0530

    na

--- parent/google/src/main/java/com/stox/google/data/bar/GoogleBarDownloadManager.java
+++ parent/google/src/main/java/com/stox/google/data/bar/GoogleBarDownloadManager.java
@@ -62,13 +62,13 @@ public class GoogleBarDownloadManager {
 			busy = true;
 			taskExecutor.schedule(() -> {
 				try {
-					//download();
+					download();
 				} catch (final Exception e) {
 					e.printStackTrace();
 				} finally {
 					busy = false;
 				}
-			}, 30, TimeUnit.SECONDS);
+			}, 3, TimeUnit.SECONDS);
 		}
 	}
 
